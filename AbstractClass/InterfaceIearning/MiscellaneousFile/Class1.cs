687638f326e4cf261350cc0517c6ae2d974e9d04
顾我 <Freedom_mr.tang@outlook.com>
2019-12-21 15:56:48 +0800

    解决方案重命名，依赖关系示列项目添加

---
+++ AbstractClass/InterfaceIearning/MiscellaneousFile/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MiscellaneousFile
+{
+    public class Class1
+    {
+    }
+}
---
+++ AbstractClass/RelyOnRelation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelyOnRelation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

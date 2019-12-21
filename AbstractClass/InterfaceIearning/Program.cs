d1e25aef11fad8c52bf71404eb829d92f8258502
顾我 <Freedom_mr.tang@outlook.com>
2019-12-21 15:48:10 +0800

    接口工作示例

--- AbstractClass/InterfaceIearning/Program.cs
+++ AbstractClass/InterfaceIearning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace InterfaceIearning
 {
@@ -6,7 +7,31 @@ namespace InterfaceIearning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] num1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            ArrayList array = new ArrayList { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(Sum(num1));
+            Console.WriteLine(Avg(array));
+        }
+
+        static int Sum(IEnumerable nums)
+        {
+            int sum = 0;
+            foreach (var item in nums)
+            {
+                sum += (int)item;
+            }
+            return sum;
+        }
+
+        static double Avg(IEnumerable nums)
+        {
+            int sum = 0; double count = 0;
+            foreach (var item in nums)
+            {
+                sum += (int)item; count++;
+            }
+            return sum / count;
         }
     }
 }
+

f4322f12ec7f611a774852b9299e44a34523036a
顾我 <Freedom_mr.tang@outlook.com>
2019-12-21 15:17:22 +0800

    抽象类学习完成

--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -6,8 +6,62 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IVehicle vehicle = new Car();
+            vehicle.Run();
+        }
 
+    }
+
+    interface IVehicle
+    {
+        void Stop();
+        
+        void Run();
+    }
+
+    abstract class Vehicle: IVehicle
+    {
+        public abstract void Run();
+
+        public abstract void Stop();
+       
+    }
+
+    class Car: IVehicle
+    {
+        public  void Run() 
+        {
+            Console.WriteLine("Car is Running");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped");
+        }
+    }
+    class Truck : IVehicle
+    {
+        public  void Run()
+        {
+            Console.WriteLine("Truck is Running");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped");
+        }
+    }
+
+    class RaceCar : IVehicle
+    {
+        public  void Run()
+        {
+            Console.WriteLine("RaceCar is Running");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped");
         }
     }
 }
